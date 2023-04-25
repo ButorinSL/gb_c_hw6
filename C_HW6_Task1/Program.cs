a27d92713030549d9dad2a7179e113504ee7829f
Sergey_Butorin <ButorinSL@gmail.com>
2023-04-25 20:44:14 +0500

    first commit

---
+++ C_HW6_Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// Функция запроса и считывания данных введенных пользователем
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+//Функция создания массива с указанием длинны (количества элементов) для заполнения его пользователем
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];    // объявляем массив с указанием длинны
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = ReadInt($"Введите значение элемента A{i + 1} = ");
+    }
+    return array;
+}
+
+
+// Функция вывод на печать массива с указанием значений для каждого элемента
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Значение элемента массива A{i +1} = {array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+//Функция поиска количества положительных чисел в массиве
+int PositiveNumber(int[] array)
+
+{
+    int positive = 0;
+    for (int i = 0; i < array.Length; i = i + 1)
+    {
+        if (array[i] > 0)
+        {
+            positive++;
+        }
+    }
+    return positive;
+}
+
+int length = ReadInt("Введите размер массива = ");
+int[] array = CreateArray(length);
+System.Console.WriteLine();
+System.Console.WriteLine("В введенном Вами массиве:");
+PrintArray(array);
+System.Console.WriteLine($"{PositiveNumber(array)} элементов больше 0");
---
+++ C_HW6_Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+// Функция запроса и считывания данных введенных пользователем
+double ReadDouble(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return double.Parse(Console.ReadLine());
+}
+
+double b1 = ReadDouble("Введите b1");
+double k1 = ReadDouble("Введите к1");
+double b2 = ReadDouble("Введите b2");
+double k2 = ReadDouble("Введите к2");
+
+if(b1 == b2)
+{
+    System.Console.WriteLine("Прямые параллельны.");
+}
+else
+{
+double x = (b2 - b1) / (k1-k2);
+double y = (k1 * x + b1);
+    System.Console.WriteLine($"Прямые пересекутся в точке с координатами Х = {x}, Y = {y}.");
+}
+
